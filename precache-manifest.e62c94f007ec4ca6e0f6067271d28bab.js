80b84eeb03ce390ebfd96a6f20697add3753aefa
selimaakid1 <selima.akid@yahoo.com>
2020-09-19 17:17:22 +0100

    Updates

---
+++ precache-manifest.e62c94f007ec4ca6e0f6067271d28bab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61ace3b9800f5b977d25811d553a92a",
+    "url": "/JerkDeFilet/index.html"
+  },
+  {
+    "revision": "dda3f5380147e351f596",
+    "url": "/JerkDeFilet/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "e538a204b089f6449355",
+    "url": "/JerkDeFilet/static/css/main.50574367.chunk.css"
+  },
+  {
+    "revision": "dda3f5380147e351f596",
+    "url": "/JerkDeFilet/static/js/2.c88382cc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/JerkDeFilet/static/js/2.c88382cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e538a204b089f6449355",
+    "url": "/JerkDeFilet/static/js/main.e31f8234.chunk.js"
+  },
+  {
+    "revision": "9b40d38ea572f96af902",
+    "url": "/JerkDeFilet/static/js/runtime-main.9c249f8e.js"
+  },
+  {
+    "revision": "d34ff8115a1824bb20807ad0a6d3c714",
+    "url": "/JerkDeFilet/static/media/JerkdeFilet1.ai.d34ff811.svg"
+  }
+]);
